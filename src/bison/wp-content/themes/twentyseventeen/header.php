3271aaf89bc8a4acba9810a2c86b971b876b7623
Jonathan Putney <jputney@noverant.com>
2017-02-25 17:18:00 -0500

    Moving to subdirectory

--- src/bison/wp-content/themes/twentyseventeen/header.php
+++ src/bison/wp-content/themes/twentyseventeen/header.php
@@ -17,7 +17,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="refresh" content="0;URL='http://bison.putney.io/gallery.html'" />
+<meta http-equiv="refresh" content="0;URL='gallery.html'" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
